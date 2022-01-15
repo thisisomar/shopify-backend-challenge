d97cc215bae8d6f4ecb5ce04cffaf9ece710e4fb
thisisomar <abusamro@sheridancollege.ca>
2022-01-15 17:05:36 -0500

    Woops, API url should be prefixed with NEXT_PUBLIC

--- frontend/src/utils/createUrqlClient.ts
+++ frontend/src/utils/createUrqlClient.ts
@@ -50,7 +50,7 @@ const urqlConfig: NextUrqlClientConfig = (ssrExchange) => {
   ];
 
   return {
-    url: process.env.GRAPHQL_API_URL || "http://localhost:4000/graphql",
+    url: process.env.NEXT_PUBLIC_GRAPHQL_API_URL || 'http://localhost:4000/api',
     exchanges
   }
 }
